18d1113aa3a2708c65d3fd934925f2f2ed979d1c
evmn <38780274+evmn@users.noreply.github.com>
2020-09-17 16:32:30 +0800

    使用Python计算止盈止损

--- index.md
+++ index.md
@@ -1,4 +1,4 @@
-# 供应和需求 - 
+# 供应和需求
 
 
  - [The Dollar, In Real Money Terms(从真实价值的角度谈美元)](Supply_and_Demand/The_Dollar_In_Real_Money_Terms) p62
@@ -8,3 +8,7 @@
  - [The Straight Answers to the Most Frequently Asked Questions(一些最常见问题的答案)](Supply_and_Demand/The_Straight_Answers_to_the_Most_Frequently_Asked_Questions) p198
 
  - [Losing to Win, It's All in Your Perception(输赢一念间)](Supply_and_Demand/Losing_to_Win/) p223
+
+---
+
+ - [使用Python计算止盈止损](scripts/)
---
+++ scripts/trade.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+while True:
+	enter = float(input("入场价: "))
+	isLong = input("开多输入y，其它都是空: ")
+	risk = float(input("风险率，建议2.0以下: "))
+	rr = [3,5,8,10]#Reward/Risk
+	tp = []#Take Profit
+	if isLong == "y":
+		for i in rr:
+			tp.append(enter*(1.0+i*risk*0.01))
+		
+		sl = enter*(1-0.01*risk)
+		print("开多:  {:9.3f}$".format(enter))
+	else:
+		for i in rr:
+			tp.append(enter*(1.0-i*risk*0.01))
+		
+		sl = enter*(1+0.01*risk)
+		print("开空:  {:9.3f}$".format(enter))
+	
+	print("盈亏比:{:10d} {:10d} {:10d} {:10d}".format(rr[0],rr[1],rr[2],rr[3]))
+	print("利润率:{:9.1f}% {:9.1f}% {:9.1f}% {:9.1f}%".format(rr[0]*risk,rr[1]*risk,rr[2]*risk,rr[3]*risk))
+	print("止盈:  {:9.3f}$ {:9.3f}$ {:9.3f}$ {:9.3f}$".format(tp[0],tp[1],tp[2],tp[3]))
+	print("止损:  {:9.3f}$\n\n".format(sl))
+	
